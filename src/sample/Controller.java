181506480f535625dd2d51ec54b5b1ea3cde8a7c
TarasVolianskyi <volianskyitv@gmail.com>
2017-09-25 13:47:59 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,157 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    public Label lbRes;
+    Helper myHelper = new Helper();
+    String nameOfDrink;
+    private int priceOfDrink;
+    private int summOfPuttingCash;
+    int change = summOfPuttingCash - priceOfDrink;
+
+    public String getNameOfDrink() {
+        return nameOfDrink;
+    }
+
+    public void setNameOfDrink(String nameOfDrink) {
+        this.nameOfDrink = nameOfDrink;
+    }
+
+    public int getPriceOfDrink() {
+        return priceOfDrink;
+    }
+
+    public void setPriceOfDrink(int priceOfDrink) {
+        this.priceOfDrink = priceOfDrink;
+    }
+
+    public int getSummOfPuttingCash() {
+        return summOfPuttingCash;
+    }
+
+    public void setSummOfPuttingCash(int summOfPuttingCash) {
+        this.summOfPuttingCash = summOfPuttingCash;
+    }
+
+    public void getStart(ActionEvent actionEvent) {
+        System.out.println(change);
+        if (summOfPuttingCash < priceOfDrink) {
+            lbRes.setText("Your choice is - " + nameOfDrink + ", price is - " + priceOfDrink + " UAH  \n" + "Please, put more");
+        } else {
+            lbRes.setText("Your choice is - " + nameOfDrink + ", price is - " + priceOfDrink + " UAH  \n" + "Enjoy your drink\nYour cahnge is:"+change+" UAH.");
+        }
+    }
+
+
+    public void addDrink1(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink1();
+        String nameOfChoice = myHelper.getNameOfDrink1();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink2(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink2();
+        String nameOfChoice = myHelper.getNameOfDrink2();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink3(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink3();
+        String nameOfChoice = myHelper.getNameOfDrink3();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink4(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink4();
+        String nameOfChoice = myHelper.getNameOfDrink4();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink5(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink5();
+        String nameOfChoice = myHelper.getNameOfDrink5();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink6(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink6();
+        String nameOfChoice = myHelper.getNameOfDrink6();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink7(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink7();
+        String nameOfChoice = myHelper.getNameOfDrink7();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink8(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink8();
+        String nameOfChoice = myHelper.getNameOfDrink8();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink9(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink9();
+        String nameOfChoice = myHelper.getNameOfDrink9();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink10(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink10();
+        String nameOfChoice = myHelper.getNameOfDrink10();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink11(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink11();
+        String nameOfChoice = myHelper.getNameOfDrink11();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void addDrink12(ActionEvent actionEvent) {
+        int priceOfChoice = myHelper.getPriceOfDrink12();
+        String nameOfChoice = myHelper.getNameOfDrink12();
+        setPriceOfDrink(priceOfChoice);
+        setNameOfDrink(nameOfChoice);
+    }
+
+    public void puttCash1(ActionEvent actionEvent) {
+        setSummOfPuttingCash(1);
+    }
+
+    public void puttCash2(ActionEvent actionEvent) {
+        setSummOfPuttingCash(2);
+    }
+
+    public void puttCash5(ActionEvent actionEvent) {
+        setSummOfPuttingCash(5);
+    }
+
+    public void puttCash10(ActionEvent actionEvent) {
+        setSummOfPuttingCash(10);
+    }
+
+    public void puttCash20(ActionEvent actionEvent) {
+        setSummOfPuttingCash(20);
+    }
+
+    public void puttCash50(ActionEvent actionEvent) {
+        setSummOfPuttingCash(50);
+    }
+
+}
---
+++ src/sample/Helper.java
@@ -0,0 +1,125 @@
+package sample;
+
+public class Helper {
+    private final String nameOfDrink1 = "Espresso";
+    private final String nameOfDrink2 = "Americano";
+    private final String nameOfDrink3 = "Americano with milk";
+    private final String nameOfDrink4 = "Late";
+    private final String nameOfDrink5 = "Cappuchino";
+    private final String nameOfDrink6 = "Mokachino";
+    private final String nameOfDrink7 = "Latte macchiato";
+    private final String nameOfDrink8 = "Ristretto";
+    private final String nameOfDrink9 = "Hot chokolate";
+    private final String nameOfDrink10 = "Green tea";
+    private final String nameOfDrink11 = "Black tea";
+    private final String nameOfDrink12 = "Fruit tea";
+
+    private final int priceOfDrink1 = 12;
+    private final int priceOfDrink2 = 15;
+    private final int priceOfDrink3 = 18;
+    private final int priceOfDrink4 = 20;
+    private final int priceOfDrink5 = 20;
+    private final int priceOfDrink6 = 25;
+    private final int priceOfDrink7 = 22;
+    private final int priceOfDrink8 = 19;
+    private final int priceOfDrink9 = 25;
+    private final int priceOfDrink10 = 15;
+    private final int priceOfDrink11 = 15;
+    private final int priceOfDrink12 = 15;
+
+    public String getNameOfDrink1() {
+        return nameOfDrink1;
+    }
+
+    public String getNameOfDrink2() {
+        return nameOfDrink2;
+    }
+
+    public String getNameOfDrink3() {
+        return nameOfDrink3;
+    }
+
+    public String getNameOfDrink4() {
+        return nameOfDrink4;
+    }
+
+    public String getNameOfDrink5() {
+        return nameOfDrink5;
+    }
+
+    public String getNameOfDrink6() {
+        return nameOfDrink6;
+    }
+
+    public String getNameOfDrink7() {
+        return nameOfDrink7;
+    }
+
+    public String getNameOfDrink8() {
+        return nameOfDrink8;
+    }
+
+    public String getNameOfDrink9() {
+        return nameOfDrink9;
+    }
+
+    public String getNameOfDrink10() {
+        return nameOfDrink10;
+    }
+
+    public String getNameOfDrink11() {
+        return nameOfDrink11;
+    }
+
+    public String getNameOfDrink12() {
+        return nameOfDrink12;
+    }
+
+    public int getPriceOfDrink1() {
+        return priceOfDrink1;
+    }
+
+    public int getPriceOfDrink2() {
+        return priceOfDrink2;
+    }
+
+    public int getPriceOfDrink3() {
+        return priceOfDrink3;
+    }
+
+    public int getPriceOfDrink4() {
+        return priceOfDrink4;
+    }
+
+    public int getPriceOfDrink5() {
+        return priceOfDrink5;
+    }
+
+    public int getPriceOfDrink6() {
+        return priceOfDrink6;
+    }
+
+    public int getPriceOfDrink7() {
+        return priceOfDrink7;
+    }
+
+    public int getPriceOfDrink8() {
+        return priceOfDrink8;
+    }
+
+    public int getPriceOfDrink9() {
+        return priceOfDrink9;
+    }
+
+    public int getPriceOfDrink10() {
+        return priceOfDrink10;
+    }
+
+    public int getPriceOfDrink11() {
+        return priceOfDrink11;
+    }
+
+    public int getPriceOfDrink12() {
+        return priceOfDrink12;
+    }
+}
